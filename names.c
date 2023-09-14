fc2cf4019b43b98f8b81a7e4bd6f3337032d7040
Yogeshgouda Patil <136431159+phantomsoldierking@users.noreply.github.com>
2023-09-14 11:53:45 +0000

    Thu, Sep 14, 2023, 11:53 AM +00:00

--- names.c
+++ names.c
@@ -8,7 +8,7 @@ int main(void)
 
     for (int i = 0; i < 7; i++)
     {
-        if (strcmp(names[i],  "asdf") == 0)
+        if (strcmp(names[i],  "yodi") == 0)
         {
             printf("Found\n");
             return 0;
